b63234b4c9e698501505a85ce3dae7da878035ec
YuriAzathoth <iamkiller@mail.com>
2021-08-15 11:52:11 +0300

    Dropped configurator related code from Shell AngelScript binding

--- Source/Shell/ScriptAPI/ShellAPI.cpp
+++ Source/Shell/ScriptAPI/ShellAPI.cpp
@@ -39,7 +39,6 @@ void RegisterShellAPI(asIScriptEngine* engine)
 
 	RegisterSubclass<Object, Shell>(engine, "Object", "Shell");
 	RegisterSubclass<RefCounted, Shell>(engine, "RefCounted", "Shell");
-
 	RegisterMembers_Object<Shell>(engine, "Shell");
 
 	engine->RegisterObjectMethod("Shell", "void StartMainMenu()", AS_METHOD(Shell, StartMainMenu), AS_CALL_THISCALL);
@@ -55,55 +54,4 @@ void RegisterShellAPI(asIScriptEngine* engine)
 								 "void StartClient(const String&in)",
 								 AS_METHOD(Shell, StartClient),
 								 AS_CALL_THISCALL);
-
-//	engine->RegisterObjectMethod("Shell",
-//								 "const String& get_profile() const",
-//								 AS_METHOD(Shell, GetProfileName),
-//								 AS_CALL_THISCALL);
-//	engine->RegisterObjectMethod("Shell",
-//								 "void LoadProfile(const String&in)",
-//								 AS_METHODPR(Shell, LoadProfile, (const Urho3D::String&), void),
-//								 AS_CALL_THISCALL);
-//	engine->RegisterObjectMethod("Shell", "void SaveProfile() const", AS_METHOD(Shell, SaveProfile), AS_CALL_THISCALL);
-//	engine->RegisterObjectMethod("Shell",
-//								 "void CreateProfile(const String&in)",
-//								 AS_METHOD(Shell, CreateProfile),
-//								 AS_CALL_THISCALL);
-//	engine->RegisterObjectMethod("Shell",
-//								 "void RemoveProfile(const String&in)",
-//								 AS_METHOD(Shell, RemoveProfile),
-//								 AS_CALL_THISCALL);
-//
-//	engine->RegisterObjectMethod("Shell",
-//								 "String get_configFilename() const",
-//								 AS_METHOD(Shell, GetConfigFilename),
-//								 AS_CALL_THISCALL);
-//	engine->RegisterObjectMethod("Shell",
-//								 "String get_configPath() const",
-//								 AS_METHOD(Shell, GetConfigPath),
-//								 AS_CALL_THISCALL);
-//	engine->RegisterObjectMethod("Shell",
-//								 "String get_inputPath() const",
-//								 AS_METHOD(Shell, GetInputPath),
-//								 AS_CALL_THISCALL);
-//	engine->RegisterObjectMethod("Shell",
-//								 "String get_logsFilename() const",
-//								 AS_METHOD(Shell, GetLogsFilename),
-//								 AS_CALL_THISCALL);
-//	engine->RegisterObjectMethod("Shell",
-//								 "String get_logsPath() const",
-//								 AS_METHOD(Shell, GetLogsPath),
-//								 AS_CALL_THISCALL);
-//	engine->RegisterObjectMethod("Shell",
-//								 "String get_pluginsPath() const",
-//								 AS_METHOD(Shell, GetPluginsPath),
-//								 AS_CALL_THISCALL);
-//	engine->RegisterObjectMethod("Shell",
-//								 "String get_profileFilename() const",
-//								 AS_METHOD(Shell, GetProfileFilename),
-//								 AS_CALL_THISCALL);
-//	engine->RegisterObjectMethod("Shell",
-//								 "String get_savesPath() const",
-//								 AS_METHOD(Shell, GetSavesPath),
-//								 AS_CALL_THISCALL);
 }
