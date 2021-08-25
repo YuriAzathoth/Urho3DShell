469efc2ba3b9184caaf219a7ceec57c69e208887
YuriAzathoth <iamkiller@mail.com>
2021-08-25 14:41:45 +0300

    Reverted back LuaJIT original code format

--- Source/ThirdParty/LuaJIT/src/lj_obj.h
+++ Source/ThirdParty/LuaJIT/src/lj_obj.h
@@ -572,15 +572,9 @@ enum
 	_(lt)                                                                                                              \
 	_(le)                                                                                                              \
 	_(concat)                                                                                                          \
-	_(call) /* The following must be in ORDER ARITH. */                                                                \
-	_(add)                                                                                                             \
-	_(sub)                                                                                                             \
-	_(mul)                                                                                                             \
-	_(div)                                                                                                             \
-	_(mod)                                                                                                             \
-	_(pow)                                                                                                             \
-	_(unm) /* The following are used in the standard libraries. */                                                     \
-	_(metatable) _(tostring) MMDEF_FFI(_) MMDEF_PAIRS(_)
+	_(call)												 /* The following must be in ORDER ARITH. */                   \
+		_(add) _(sub) _(mul) _(div) _(mod) _(pow) _(unm) /* The following are used in the standard libraries. */       \
+		_(metatable) _(tostring) MMDEF_FFI(_) MMDEF_PAIRS(_)
 
 typedef enum
 {
