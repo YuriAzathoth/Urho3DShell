30974ad00d1fd223c764fc1b4feb9f270e535b15
YuriAzathoth <iamkiller@mail.com>
2021-09-03 14:54:35 +0300

    Revert accidentally commited ShellApplication debug version

--- Source/Shell/Core/ShellApplication.cpp
+++ Source/Shell/Core/ShellApplication.cpp
@@ -74,7 +74,6 @@ void ShellApplication::Start()
 	const auto itScript = shellParameters_.Find(SP_SCRIPT);
 	if (itScript != shellParameters_.End())
 		plugins->Load(itScript->second_.GetString());
-	plugins->Load("test.as");
 
 	GetSubsystem<Config>()->Apply(shellParameters_);
 }
